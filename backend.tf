2a1ce6d05e5995578c63d1097432cd7f4e8b6eb9
Nithin Prince John <nithinpj09@gmail.com>
2023-12-11 11:58:07 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
+#terraform {
+  #backend "s3" {
     # bucket = "npj-terraform-state"
     # key    = "networking/terraform.tfstate"
     # region = "eu-west-3"
-  }
-}
+  #}
+#}
